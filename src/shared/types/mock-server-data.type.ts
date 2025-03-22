ccd757aee01fbff49a2b4417a754db5629627e42
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2025-03-22 15:15:23 +0500

    Добавила вспомогательные функции и тип для мокового сервера

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,11 @@
+export type MockServerData = {
+  titles: string[];
+  summaries: string[];
+  previewPathes: string[];
+  photos: string[];
+  authors: string[];
+  commentCounts: string[];
+  costs: string[];
+  facilities: string[];
+  coordinates: string[];
+};
