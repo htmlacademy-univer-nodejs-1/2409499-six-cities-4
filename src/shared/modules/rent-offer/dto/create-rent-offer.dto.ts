ce73e3dd2b12aad781de6e11b5f5b4c515109895
Shmantsar Angelina <Angelina.Shmantsar@urfu.me>
2025-05-12 01:54:35 +0500

    Закончила задание

--- src/shared/modules/rent-offer/dto/create-rent-offer.dto.ts
+++ src/shared/modules/rent-offer/dto/create-rent-offer.dto.ts
@@ -1,25 +1,65 @@
-import { City } from '../../../types/city.enum.js';
-import { Coordinates } from '../../../types/coordinates.type.js';
-import { Facility } from '../../../types/facility.enum.js';
-import { RentOfferType } from '../../../types/rent-offer-type.enum.js';
+import { IsArray, IsBoolean, IsDateString, IsInt, IsMongoId, IsNumber, IsObject, IsString, Length, MaxLength, MinLength } from 'class-validator';
 import { User } from '../../../types/user.type.js';
+import { CreateOfferValidationMessage } from './create-messages.js';
+import { RentOfferType } from '../../../types/rent-offer-type.enum.js';
+import { Facility } from '../../../types/facility.enum.js';
+import { Coordinates } from '../../../types/coordinates.type.js';
+import { City } from '../../../types/city.enum.js';
 
 export default class CreateRentOfferDto {
-  public title: string;
-  public summary: string;
-  public publicationDate: Date;
-  public city: City;
-  public previewPath: string;
-  public photos: string[];
-  public isPremium: boolean;
-  public isFavorite: boolean;
-  public rating: 1 | 2 | 3 | 4 | 5;
-  public offerType: RentOfferType;
-  public roomCount: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-  public guestCount: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-  public cost: number;
-  public facilities: Facility[];
-  public author: User;
-  public commentCount: number;
-  public coordinates: Coordinates;
+  @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
+  @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
+    title!: string;
+
+  @MinLength(20, { message: CreateOfferValidationMessage.summary.minLength })
+  @MaxLength(1024, { message: CreateOfferValidationMessage.summary.maxLength })
+    summary!: string;
+
+  @IsDateString({}, {message: CreateOfferValidationMessage.publicationDate.invalidFormat})
+    publicationDate!: Date;
+
+  @IsString({message: CreateOfferValidationMessage.city.invalidFormat})
+    city!: City;
+
+  @IsString({message: CreateOfferValidationMessage.previewPath.invalidFormat})
+    previewPath!: string;
+
+  @IsArray({message: CreateOfferValidationMessage.photos.invalidFormat})
+    photos!: string[];
+
+  @IsBoolean({message: CreateOfferValidationMessage.isPremium.invalidFormat})
+    isPremium!: boolean;
+
+  @IsBoolean({message: CreateOfferValidationMessage.isFavorite.invalidFormat})
+    isFavorite!: boolean;
+
+  @IsNumber({}, {message: CreateOfferValidationMessage.rating.invalidFormat})
+  @Length(1, 5, {message: CreateOfferValidationMessage.rating.lengthField})
+    rating!: 1 | 2 | 3 | 4 | 5;
+
+  @IsString({message: CreateOfferValidationMessage.offerType.invalidFormat})
+    offerType!: RentOfferType;
+
+  @IsInt({message: CreateOfferValidationMessage.roomCount.invalidFormat})
+  @Length(1, 8, {message: CreateOfferValidationMessage.roomCount.lengthField})
+    roomCount!: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+  @IsInt({message: CreateOfferValidationMessage.guestCount.invalidFormat})
+  @Length(1, 10, {message: CreateOfferValidationMessage.guestCount.lengthField})
+    guestCount!: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+  @IsNumber({}, {message: CreateOfferValidationMessage.cost.invalidFormat})
+  @Length(100, 100000, {message: CreateOfferValidationMessage.cost.lengthField})
+    cost!: number;
+
+  @IsArray({message: CreateOfferValidationMessage.facilities.invalidFormat})
+    facilities!: Facility[];
+
+  @IsMongoId({ message: CreateOfferValidationMessage.author.invalidId })
+    author!: User;
+
+  commentCount!: number;
+
+  @IsObject({message:CreateOfferValidationMessage.coordinates.invalidFormat})
+    coordinates!: Coordinates;
 }
--- src/shared/modules/rent-offer/dto/update-offer.dto.ts
+++ src/shared/modules/rent-offer/dto/update-offer.dto.ts
@@ -1,25 +1,82 @@
-import { City } from '../../../types/city.enum.js';
-import { Coordinates } from '../../../types/coordinates.type.js';
-import { Facility } from '../../../types/facility.enum.js';
-import { RentOfferType } from '../../../types/rent-offer-type.enum.js';
+import { IsArray, IsBoolean, IsDateString, IsInt, IsMongoId, IsNumber, IsObject, IsOptional, IsString, Length, MaxLength, MinLength } from 'class-validator';
 import { User } from '../../../types/user.type.js';
+import { CreateOfferValidationMessage } from './create-messages.js';
+import { City } from '../../../types/city.enum.js';
+import { RentOfferType } from '../../../types/rent-offer-type.enum.js';
+import { Facility } from '../../../types/facility.enum.js';
+import { Coordinates } from '../../../types/coordinates.type.js';
 
 export default class UpdateOfferDto {
-  title?: string;
-  summary?: string;
-  publicationDate?: Date;
-  city?: City;
-  previewPath?: string;
-  photos?: string[];
-  isPremium?: boolean;
-  isFavorite?: boolean;
-  rating?: 1 | 2 | 3 | 4 | 5;
-  offerType?: RentOfferType;
-  roomCount?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-  guestCount?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-  cost?: number;
-  facilities?: Facility[];
-  author?: User;
-  commentCount?: number;
-  coordinates?: Coordinates;
+  @IsOptional()
+  @MinLength(10, { message: CreateOfferValidationMessage.title.minLength })
+  @MaxLength(100, { message: CreateOfferValidationMessage.title.maxLength })
+    title?: string;
+
+  @IsOptional()
+  @MinLength(20, { message: CreateOfferValidationMessage.summary.minLength })
+  @MaxLength(1024, { message: CreateOfferValidationMessage.summary.maxLength })
+    summary?: string;
+
+  @IsOptional()
+  @IsDateString({}, {message: CreateOfferValidationMessage.publicationDate.invalidFormat})
+    publicationDate?: Date;
+
+  @IsOptional()
+  @IsString({message: CreateOfferValidationMessage.city.invalidFormat})
+    city?: City;
+
+  @IsOptional()
+  @IsString({message: CreateOfferValidationMessage.previewPath.invalidFormat})
+    previewPath?: string;
+
+  @IsOptional()
+  @IsArray({message: CreateOfferValidationMessage.photos.invalidFormat})
+    photos?: string[];
+
+  @IsOptional()
+  @IsBoolean({message: CreateOfferValidationMessage.isPremium.invalidFormat})
+    isPremium!: boolean;
+
+  @IsOptional()
+  @IsBoolean({message: CreateOfferValidationMessage.isFavorite.invalidFormat})
+    isFavorite?: boolean;
+
+  @IsOptional()
+  @IsNumber({}, {message: CreateOfferValidationMessage.rating.invalidFormat})
+  @Length(1, 5, {message: CreateOfferValidationMessage.rating.lengthField})
+    rating?: 1 | 2 | 3 | 4 | 5;
+
+  @IsOptional()
+  @IsString({message: CreateOfferValidationMessage.offerType.invalidFormat})
+    offerType?: RentOfferType;
+
+  @IsOptional()
+  @IsInt({message: CreateOfferValidationMessage.roomCount.invalidFormat})
+  @Length(1, 8, {message: CreateOfferValidationMessage.roomCount.lengthField})
+    roomCount?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
+
+  @IsOptional()
+  @IsInt({message: CreateOfferValidationMessage.guestCount.invalidFormat})
+  @Length(1, 10, {message: CreateOfferValidationMessage.guestCount.lengthField})
+    guestCount?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+  @IsOptional()
+  @IsNumber({}, {message: CreateOfferValidationMessage.cost.invalidFormat})
+  @Length(100, 100000, {message: CreateOfferValidationMessage.cost.lengthField})
+    cost?: number;
+
+  @IsOptional()
+  @IsArray({message: CreateOfferValidationMessage.facilities.invalidFormat})
+    facilities?: Facility[];
+
+  @IsOptional()
+  @IsMongoId({ message: CreateOfferValidationMessage.author.invalidId })
+    author?: User;
+
+  @IsOptional()
+    commentCount?: number;
+
+  @IsOptional()
+  @IsObject({message:CreateOfferValidationMessage.coordinates.invalidFormat})
+    coordinates?: Coordinates;
 }
